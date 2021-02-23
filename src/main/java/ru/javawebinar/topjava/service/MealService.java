629a8155482100187eba3ee16735e650f4f7071c
Yuri <kopnin_u@mail.ru>
2021-02-23 11:41:58 -0800

    finish of HW03 without optional

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -15,7 +15,7 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 @Service
 public class MealService {
 
-    private final MealRepository repository;
+    private final MealRepository  repository;
 
     public MealService(MealRepository repository) {
         this.repository = repository;
